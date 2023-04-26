028724808060d9b76241c617cbff38d83a4a82f7
Franco Lepe <Francolepe169@gmail.com>
2023-04-26 16:33:34 -0400

    done

--- server/models.py
+++ server/models.py
@@ -12,5 +12,23 @@ class Newsletter(db.Model, SerializerMixin):
     published_at = db.Column(db.DateTime, server_default=db.func.now())
     edited_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    def patch(self, id):
+
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response   
+
     def __repr__(self):
         return f'<Newsletter {self.title}, published at {self.published_at}.>'
